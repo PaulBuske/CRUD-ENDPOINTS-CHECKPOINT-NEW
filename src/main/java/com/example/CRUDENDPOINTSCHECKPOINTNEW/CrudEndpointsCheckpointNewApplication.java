9eeb8610853079ee26866d7f0c26e059a6f4e9f8
PaulBuske <paul.a.buske.mil@swf.army.mil>
2021-09-28 15:32:38 -0500

    initial commit

---
+++ src/main/java/com/example/CRUDENDPOINTSCHECKPOINTNEW/CrudEndpointsCheckpointNewApplication.java
@@ -0,0 +1,13 @@
+package com.example.CRUDENDPOINTSCHECKPOINTNEW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudEndpointsCheckpointNewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudEndpointsCheckpointNewApplication.class, args);
+	}
+
+}
